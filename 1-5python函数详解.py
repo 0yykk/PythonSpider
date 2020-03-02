03ad4beb27215c75302740b4348944664ee3e88e
0yykk <921122271@qq.com>
2020-03-02 15:35:31 +0800

    函数详解

---
+++ 1-5python函数详解.py
@@ -0,0 +1,16 @@
+#OOP面向对象编程
+
+#函数定义格式
+def func():
+    j=10
+    print(j)
+#调用函数：函数名（参数）
+func()
+
+#使用参数
+def func2(a,b):
+    if(a>b):
+        print(str(a)+">"+str(b))
+    else:
+        print(str(b)+">"+str(a))
+func2(2,3)
