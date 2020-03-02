900879ece5c2ff649ce09c0eefafa6963e7474c1
0yykk <921122271@qq.com>
2020-03-02 16:00:31 +0800

    模块详解

---
+++ 1-6python模块实战.py
@@ -0,0 +1,18 @@
+#什么是Pyhton模块：函数组合在一起形成模块，系统自带的模块在安装目录的lib中
+#导入
+#import 模块名
+#from。。。import。。。
+'''
+import cgi
+或者
+from cgi import closelog()
+模块的类别
+    1.自带模块
+    2.第三方模块
+    3.自定义模块
+第三方模块安装
+    1.使用pip
+    2.whl下载安装
+    3.直接复制
+    4.anaconda（模块包的管理工具）
+'''
